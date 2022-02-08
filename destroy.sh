c0f5c79d2fdf87c6561db370429b721a3b9acbde
henriklagergren <henriklagergren@hotmail.com>
2022-02-08 12:50:34 +0100

    update of destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -1,19 +1,22 @@
-cd http/
-
-pulumi stack select trigger
-
-pulumi destroy -f -y
+# Destroy HTTP trigger
+cd http/ && pulumi destroy -f -y
 
 cd ..
-cd shared/
 
-pulumi stack select shared
-
-pulumi destroy -f -y
+# Destroy storage trigger
+cd storage/ && pulumi destroy -f -y
 
 cd ..
-cd infra/
 
-pulumi stack select infra
+# Destroy queue trigger
+cd queue/ && pulumi destroy -f -y
 
-pulumi destroy -f -y
+cd ..
+
+# Destroy infrastructure
+cd infra/ && pulumi destroy -f -y
+
+cd ..
+
+# Destroy shared resources (must be done last due to how pulumi tracks resources)
+cd shared/ && pulumi destroy -f -y
